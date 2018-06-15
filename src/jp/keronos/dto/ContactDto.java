d9e6bef257bf2d074364c7071623277fab298527
Owner <Owner@AB3-05>
2018-06-15 11:16:31 +0900

    Merge ContactDetailDto to ContactDto

--- src/jp/keronos/dto/ContactDto.java
+++ src/jp/keronos/dto/ContactDto.java
@@ -1,24 +1,76 @@
 package jp.keronos.dto;
 
+import java.sql.Timestamp;
+
 public class ContactDto {
 
-	private int contactId;
-	
-	private int userId;
-	
-	public int getContactId() {
-		return contactId;
-	}
+    private int contactDetailId;
 
-	public void setContactId(int contactId) {
-		this.contactId = contactId;
-	}
+    private int contactId;
 
-	public int getUserId() {
-		return userId;
-	}
+    private String contactDetail;
 
-	public void setUserId(int userId) {
-		this.userId = userId;
-	}
+    private Timestamp contactAt;
+
+    private int requestOrResponseFlg;
+
+    private int manageNo;
+
+    private int userNo;
+
+    public int getContactDetailId() {
+        return contactDetailId;
+    }
+
+    public void setContactDetailId(int contactDetailId) {
+        this.contactDetailId = contactDetailId;
+    }
+
+    public int getContactId() {
+        return contactId;
+    }
+
+    public void setContactId(int contactId) {
+        this.contactId = contactId;
+    }
+
+    public String getContactDetail() {
+        return contactDetail;
+    }
+
+    public void setContactDetail(String contactDetail) {
+        this.contactDetail = contactDetail;
+    }
+
+    public Timestamp getContactAt() {
+        return contactAt;
+    }
+
+    public void setContactAt(Timestamp contactAt) {
+        this.contactAt = contactAt;
+    }
+
+    public int getRequestOrResponseFlg() {
+        return requestOrResponseFlg;
+    }
+
+    public void setRequestOrResponseFlg(int requestOrResponseFlg) {
+        this.requestOrResponseFlg = requestOrResponseFlg;
+    }
+
+    public int getManageNo() {
+        return manageNo;
+    }
+
+    public void setManageNo(int manageNo) {
+        this.manageNo = manageNo;
+    }
+
+    public int getUserNo() {
+        return userNo;
+    }
+
+    public void setUserNo(int userNo) {
+        this.userNo = userNo;
+    }
 }
